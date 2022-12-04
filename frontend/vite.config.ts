56c6f95b7d9b26185bd24bff3a480fdd595ef8a4
d9beuD <d9beuD@users.noreply.github.com>
2022-12-04 23:15:22 +0100

    Add Vue 2 compat module

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      vue: "@vue/compat",
+    },
+  },
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2,
+          },
+        },
+      },
+    }),
+  ],
+});
