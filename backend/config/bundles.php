624e5485d5e041aab7815d576dabfabe7e7ea9af
d9beuD <d9beuD@users.noreply.github.com>
2022-12-04 21:19:23 +0100

    Add dependencies for crud command

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
