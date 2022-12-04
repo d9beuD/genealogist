b1ab697c57e56f16646e8d063bedbf28902b45a4
d9beuD <d9beuD@users.noreply.github.com>
2022-12-04 19:58:28 +0100

    Add Symfony components

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
 ];
