83d093e2c73d6a10570e5f89fa8bf2dcbbd10963
d9beuD <d9beuD@users.noreply.github.com>
2022-12-06 14:33:04 +0100

    Add a navbar

---
+++ frontend/src/components/navbars/MainNavbar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {
+  BCollapse,
+  BNavbar,
+  BNavbarBrand,
+  BNavbarNav,
+  BNavbarToggle,
+  BNavItem,
+} from "bootstrap-vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faTreeDeciduous } from "@fortawesome/pro-duotone-svg-icons";
+</script>
+
+<template>
+  <BNavbar type="dark" variant="dark" toggleable="md">
+    <BNavbarBrand>
+      <FontAwesomeIcon :icon="faTreeDeciduous" />
+      Genealogist
+    </BNavbarBrand>
+    <BNavbarToggle target="main-navbar-collapse" />
+
+    <BCollapse id="main-navbar-collapse" is-nav>
+      <BNavbarNav>
+        <BNavItem>Home</BNavItem>
+      </BNavbarNav>
+    </BCollapse>
+  </BNavbar>
+</template>
