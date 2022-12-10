c139f770b4e15df34aa53795611d7683dd7d6bcb
d9beuD <d9beuD@users.noreply.github.com>
2022-12-10 11:42:44 +0100

    Add relationship between a tree and its members

--- backend/src/Entity/Tree.php
+++ backend/src/Entity/Tree.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TreeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TreeRepository::class)]
@@ -19,6 +21,14 @@ class Tree
     #[ORM\ManyToOne(inversedBy: 'trees')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $owner = null;
+
+    #[ORM\OneToMany(mappedBy: 'tree', targetEntity: Person::class, orphanRemoval: true)]
+    private Collection $members;
+
+    public function __construct()
+    {
+        $this->members = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +55,36 @@ class Tree
     public function setOwner(?User $owner): self
     {
         $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Person>
+     */
+    public function getMembers(): Collection
+    {
+        return $this->members;
+    }
+
+    public function addMember(Person $member): self
+    {
+        if (!$this->members->contains($member)) {
+            $this->members->add($member);
+            $member->setTree($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMember(Person $member): self
+    {
+        if ($this->members->removeElement($member)) {
+            // set the owning side to null (unless already changed)
+            if ($member->getTree() === $this) {
+                $member->setTree(null);
+            }
+        }
 
         return $this;
     }
