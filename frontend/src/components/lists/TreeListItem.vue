8815426e9ee4d3454ea97919f4cda421c5762354
d9beuD <d9beuD@users.noreply.github.com>
2022-12-10 12:38:00 +0100

    Add a TreeList component

---
+++ frontend/src/components/lists/TreeListItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Tree } from "@/api/types";
+
+interface Props {
+  tree: Tree;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="tree-list-item">
+    <h4>{{ tree.name }}</h4>
+  </div>
+</template>
