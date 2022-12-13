a36be69c665f426c4796eefb267463215342058f
d9beuD <d9beuD@users.noreply.github.com>
2022-12-13 22:17:03 +0100

    Beautify member list

--- frontend/src/components/lists/MemberListItem.vue
+++ frontend/src/components/lists/MemberListItem.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type { Person } from "@/api/types";
+import { faChevronRight } from "@fortawesome/pro-duotone-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 interface Props {
   person: Person;
@@ -9,7 +11,17 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="px-3 py-1">
-    {{ props.person.lastname }} {{ props.person.firstname }}
-  </div>
+  <RouterLink
+    class="member-list-item"
+    :to="{ name: 'showMember', params: { memberId: props.person.id } }"
+  >
+    <div class="form-row">
+      <div class="col">
+        {{ props.person.lastname }} {{ props.person.firstname }}
+      </div>
+      <div class="col-auto chevron">
+        <FontAwesomeIcon :icon="faChevronRight" swap-opacity />
+      </div>
+    </div>
+  </RouterLink>
 </template>
