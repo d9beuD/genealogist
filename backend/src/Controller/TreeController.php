77e4e4ac4d9ea0043f17236eba8f2c3319f05847
d9beuD <d9beuD@users.noreply.github.com>
2022-12-14 23:52:21 +0100

    Members list is not paginated anymore

--- backend/src/Controller/TreeController.php
+++ backend/src/Controller/TreeController.php
@@ -68,4 +68,10 @@ class TreeController extends AbstractController
         $treeRepository->remove($tree, true);
         return $this->json($tree);
     }
+
+    #[Route('/tree/{id}/members', name: 'app_tree_members', methods: ['GET'])]
+    public function getMembers(Tree $tree): Response
+    {
+        return $this->json($tree->getMembers());
+    }
 }
