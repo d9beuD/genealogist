c2b3bb16c7785a20134dc0aa7290f6e0a4719212
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-04-12 16:16:20 +0200

    chore: add PHP quality tooling

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setUnsupportedPhpVersionAllowed(true)
+    ->setFinder($finder)
+;
