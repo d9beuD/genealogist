549679ce0c7490d4ffcfb6ceba03fc79aa05cc2e
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-04-12 16:56:04 +0200

    chore: enable Rector PHP 8.4 set

--- rector.php
+++ rector.php
@@ -9,6 +9,7 @@ return RectorConfig::configure()
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
+    ->withPhpSets(php84: true)
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
