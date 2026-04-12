68f77b35264fe00d2ed877e8e898105bc2c78b6a
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-04-12 16:44:45 +0200

    refactor: apply StrlenZeroToIdenticalEmptyStringRector

--- src/Entity/Trait/FormatEmptyStringTrait.php
+++ src/Entity/Trait/FormatEmptyStringTrait.php
@@ -8,6 +8,6 @@ trait FormatEmptyStringTrait
 {
     public function formatEmptyString(?string $value): ?string
     {
-        return ($value !== null && strlen(trim($value)) === 0) ? null : $value;
+        return ($value !== null && trim($value) === '') ? null : $value;
     }
 }
