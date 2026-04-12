0debcaddff72e551660b185cd5723d575c597a15
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-04-12 13:03:00 +0200

    fix(source): allow inline editing from member profiles

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/person/{personId}/source')]
@@ -21,13 +22,14 @@ class SourceController extends AbstractController
     ) {}
     
     #[Route('/', name: 'app_source_index', methods: ['GET', 'POST'])]
+    #[IsGranted('edit', 'person')]
     public function index(Request $request, EntityManagerInterface $entityManager, #[MapEntity(id: 'personId')] Person $person): Response
     {
         $source = new Source();
-        $form = $this->createForm(SourceType::class, $source);
-        $form->handleRequest($request);
+        $createForm = $this->createForm(SourceType::class, $source);
+        $createForm->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($createForm->isSubmitted() && $createForm->isValid()) {
             $source->setPerson($person);
             $entityManager->persist($source);
             $entityManager->flush();
@@ -42,13 +44,52 @@ class SourceController extends AbstractController
         return $this->render('source/index.html.twig', [
             'sources' => $person->getSources(),
             'person' => $person,
-            'form' => $form,
+            'create_form' => $createForm->createView(),
+            'edit_form' => null,
+            'editing_source' => null,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_source_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('edit', 'person')]
+    public function edit(
+        Request $request,
+        Source $source,
+        EntityManagerInterface $entityManager,
+        #[MapEntity(id: 'personId')] Person $person,
+    ): Response {
+        $this->assertSourceBelongsToPerson($source, $person);
+
+        $createForm = $this->createForm(SourceType::class, new Source());
+        $editForm = $this->createForm(SourceType::class, $source);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans('source.edit.success')
+            );
+
+            return $this->redirectToRoute('app_source_index', ['personId' => $person->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('source/index.html.twig', [
+            'sources' => $person->getSources(),
+            'person' => $person,
+            'create_form' => $createForm->createView(),
+            'edit_form' => $editForm->createView(),
+            'editing_source' => $source,
         ]);
     }
 
     #[Route('/{id}', name: 'app_source_delete', methods: ['POST'])]
+    #[IsGranted('edit', 'person')]
     public function delete(Request $request, Source $source, EntityManagerInterface $entityManager, #[MapEntity(id: 'personId')] Person $person): Response
     {
+        $this->assertSourceBelongsToPerson($source, $person);
+
         if ($this->isCsrfTokenValid('delete'.$source->getId(), $request->request->get('_token'))) {
             $entityManager->remove($source);
             $entityManager->flush();
@@ -65,5 +106,12 @@ class SourceController extends AbstractController
         }
 
         return $this->redirectToRoute('app_source_index', ['personId' => $person->getId()], Response::HTTP_SEE_OTHER);
+    }
+
+    private function assertSourceBelongsToPerson(Source $source, Person $person): void
+    {
+        if ($source->getPerson()?->getId() !== $person->getId()) {
+            throw $this->createNotFoundException();
+        }
     }
 }
