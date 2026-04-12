ce00dd010a43819e2b5fc2e12ca09d6a7ac19c63
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-04-12 17:18:38 +0200

    chore: enable Rector Doctrine code quality set

--- rector.php
+++ rector.php
@@ -14,5 +14,6 @@ return RectorConfig::configure()
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
+        doctrineCodeQuality: true,
         symfonyCodeQuality: true,
     );
