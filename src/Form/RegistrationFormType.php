2e48598b231b3d4b8fa57f33c140f68df3080b6f
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-04-12 16:44:42 +0200

    refactor: apply SortCallLikeNamedArgsRector

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -51,9 +51,9 @@ class RegistrationFormType extends AbstractType
                     new NotBlank(message: 'Please enter a password.'),
                     new Length(
                         min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters.',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
+                        minMessage: 'Your password should be at least {{ limit }} characters.',
                     ),
                 ],
                 'label' => 'form.field.password',
