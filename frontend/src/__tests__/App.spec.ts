a849f205d0b7874d16fd016114b362ea83441d42
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-06-04 17:33:47 +0200

    build(frontend): Add form validation dependencies

--- frontend/src/__tests__/App.spec.ts
+++ frontend/src/__tests__/App.spec.ts
@@ -2,10 +2,19 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
+import router from '../router'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+  it('mounts renders properly', async () => {
+    router.push('/')
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router],
+      },
+    })
+
+    expect(wrapper.exists()).toBe(true)
   })
 })
